0090b65785b2ec82c443010f7069c8dba9e93139
Dinçer Uygun <dncruygun@hotmail.com>
2015-09-09 15:10:01 +0300

    denemeler sürüyor

---
+++ Deneme/deneme/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
